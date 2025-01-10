25a692a5614b4296bbac3ecb1d48d07026709523
restlucas <lucasouza.doliveira@gmail.com>
2025-01-10 14:18:35 -0300

    feat: vercel storage configuration

--- src/app/(home)/login/options.tsx
+++ src/app/(home)/login/options.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import { FacebookLogo, GoogleLogo } from "@phosphor-icons/react";
+import { GoogleLogo } from "@phosphor-icons/react";
 import { signIn } from "next-auth/react";
 
 export function LoginOptions() {
   return (
     <div className="grid grid-cols-1 gap-4">
-      <button
+      {/* <button
         onClick={() => signIn("facebook")}
         className="py-2 rounded-md bg-facebook flex items-center justify-center gap-2 duration-100 hover:bg-facebook/50"
       >
         <FacebookLogo size={20} weight="bold" />
         <span className="font-bold">Facebook</span>
-      </button>
+      </button> */}
       <button
         onClick={() => signIn("google")}
         className="py-2 rounded-md bg-google flex items-center justify-center gap-2 duration-100 hover:bg-google/50"
