3524d43409e8fffac4a29dcf8206d892f17a976d
restlucas <lucasouza.doliveira@gmail.com>
2025-01-17 00:29:48 -0300

    fix: adding suspense

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Couplespace",
@@ -15,7 +16,9 @@ export default function RootLayout({
   return (
     <>
       <Header />
-      <main className="mx-8 flex-1">{children}</main>
+      <Suspense>
+        <main className="mx-8 flex-1">{children}</main>
+      </Suspense>
       <Footer />
     </>
   );
